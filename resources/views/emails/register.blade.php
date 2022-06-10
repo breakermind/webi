23399f6304bf58bc5b3d26f2a4aa3abafb44bf86
breakermind <m.luk.box@gmail.com>
2022-06-10 17:09:23 +0200

    Update

--- resources/views/emails/register.blade.php
+++ resources/views/emails/register.blade.php
@@ -3,5 +3,5 @@
 </div>
 <div class="row">
 	<p> @lang('Confirm your email address') </p>
-	<a href="{{ request()->getSchemeAndHttpHost() }}/web/api/activate/{{ $user->id }}/{{ $user->code }}" target="_blank"> @lang('Confirm email') </a>
+	<a href="{{ request()->getSchemeAndHttpHost() }}/activate/{{ $user->id }}/{{ $user->code }}" target="_blank"> @lang('Confirm email') </a>
 </div>
